b283bea57dbd12594a88e9ff78dd23be378ae087
galymbekBuilder <amanzhangalymbek4@gmail.com>
2025-10-25 04:40:03 +0600

    Мои изменения

--- sw.js
+++ sw.js
@@ -1,12 +1,17 @@
-const CACHE='id-docs-pwa-v1';
-const ASSETS=['./','./index.html','./manifest.json','./icon-192.png','./icon-512.png'];
-self.addEventListener('install',e=>{e.waitUntil(caches.open(CACHE).then(c=>c.addAll(ASSETS))); self.skipWaiting();});
-self.addEventListener('activate',e=>{e.waitUntil(self.clients.claim());});
-self.addEventListener('fetch',e=>{
-  const url=new URL(e.request.url);
-  if(url.pathname.endsWith('.html') || url.pathname==='/'){
-    e.respondWith(fetch(e.request).then(r=>{const copy=r.clone(); caches.open(CACHE).then(c=>c.put(e.request,copy)); return r;}).catch(()=>caches.match(e.request)));
-  }else{
-    e.respondWith(caches.match(e.request).then(r=>r||fetch(e.request)));
-  }
+const CACHE = 'digital-docs-v1';
+const ASSETS = [
+  './',
+  './index.html',
+  './manifest.json'
+];
+self.addEventListener('install', (e)=>{
+  e.waitUntil(caches.open(CACHE).then(c=>c.addAll(ASSETS)).then(()=>self.skipWaiting()));
+});
+self.addEventListener('activate', (e)=>{
+  e.waitUntil(self.clients.claim());
+});
+self.addEventListener('fetch', (e)=>{
+  e.respondWith(
+    caches.match(e.request).then(r => r || fetch(e.request))
+  );
 });
